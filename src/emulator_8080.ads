d3c2eea2774e2b52060230bb35be724ec79cc83d
Lukas H <lukas.holzenkaempfer@kabelmail.de>
2020-04-14 20:36:20 +0200

    Delete unused type declaration

--- src/emulator_8080.ads
+++ src/emulator_8080.ads
@@ -3,5 +3,4 @@ package Emulator_8080 is
    for Byte_Type'Size use 8;
 
    type Byte_Array_Type is array(Natural range <>) of Byte_Type;
-   type Ram_Type is range 0 .. 16#FFFF#;
 end Emulator_8080;
