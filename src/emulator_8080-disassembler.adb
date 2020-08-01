129c02d01d108692a547b1f3cc72ef92aa54f351
Woodfighter <lukas.holzenkaempfer@kabelmail.de>
2020-08-01 02:51:44 +0200

    Add functio sceleton RST_4

--- src/emulator_8080-disassembler.adb
+++ src/emulator_8080-disassembler.adb
@@ -597,6 +597,8 @@ package body Emulator_8080.Disassembler is
             when 16#e6# =>
                Emulator_8080.Processor.ANI_D8(Byte_2    => Processor.Memory(Processor.Program_Counter + 1),
                                               Processor => Processor);
+            when 16#e7# =>
+               Emulator_8080.Processor.RST_4(Processor => Processor);
             when others =>
                Emulator_8080.Processor.Unimplemented_Instruction(Processor);
          end case;
--- src/emulator_8080-processor.adb
+++ src/emulator_8080-processor.adb
@@ -2735,6 +2735,18 @@ package body Emulator_8080.Processor is
                          Exception_Cause   => GNAT.Current_Exception.Exception_Information);
    end ANI_D8;
 
+   procedure RST_4(Processor : in out Processor_Type) is
+   begin
+      Ada.Text_IO.Put_Line("RST 4!");
+      CALL(Byte_2    => 0,
+           Byte_3    => 0,
+           Processor => Processor);
+   exception
+      when others =>
+         Print_Exception(Throwing_Function => GNAT.Source_Info.Enclosing_Entity,
+                         Exception_Cause   => GNAT.Current_Exception.Exception_Information);
+   end RST_4;
+
    procedure Unimplemented_Instruction(Processor : in out Processor_Type) is
    begin
       --Ada.Text_IO.Put_Line("Not yet implemented");
