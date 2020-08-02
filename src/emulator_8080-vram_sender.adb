d1b0b4f0f2a599df30a31cb6fdf438e1efbc20b4
Woodfighter <lukas.holzenkaempfer@kabelmail.de>
2020-08-03 00:31:58 +0200

    Add vram_sender

--- src/emulator_8080-vram_sender.adb
+++ src/emulator_8080-vram_sender.adb
@@ -3,6 +3,10 @@ with Ada.Text_IO;
 package body Emulator_8080.Vram_Sender is
    procedure Send_Vram(Vram : in Emulator_8080.Processor.Vram_Type) is
    begin
-      Ada.Text_IO.Put_Line("TESTITEST CALLBACK!");
+      for I in Vram'Range loop
+         if Vram(I) /= 0 then
+            Ada.Text_IO.Put_Line(Vram(I)'Img);
+         end if;
+      end loop;
    end;
 end Emulator_8080.Vram_Sender;
